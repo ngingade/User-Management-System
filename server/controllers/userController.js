4095a6a809ff92c222e0212a9486225e4965f816
ngingade <naveenbirur@gmail.com>
2024-08-27 21:33:55 +0530

    Implemented input validation for user creation endpoint to prevent invalid data, including: Firstname field: only alphabetical characters allowed and max 100 characters Lastname field: only alphabetical characters allowed  and max 100 characters Email field: must be a valid email address

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
 class UserController {
   async getAllUsers(req, res) {
@@ -11,12 +12,30 @@ class UserController {
   }
 
   async createUser(req, res) {
+    await body("firstName")
+      .isLength({ max: 100 })
+      .withMessage("Max 100 characters are allowed")
+      .isAlpha()
+      .withMessage("Firstname should only contain alphabetical characters")
+      .run(req);
+    await body("lastName")
+      .isLength({ max: 100 })
+      .withMessage("Max 100 characters are allowed")
+      .isAlpha()
+      .withMessage("Lastname should only contain alphabetical characters")
+      .run(req);
+    await body("email").isEmail().withMessage("Invalid email address").run(req);
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
     try {
       const user = new User(req.body);
       await user.save();
       res.send(user);
     } catch (err) {
-      res.status(500).send(err);
+      res.status(500).send({ message: "Failed to create user" });
     }
   }
 }
